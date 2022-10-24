c894f4a253fc760cae5f17fd06350993933850db
hhw12409 <sksk2425@naver.com>
2022-10-25 00:31:29 +0900

    Feat: onLikeClick function

--- src/components/AttractionsList/AttractionsList.js
+++ src/components/AttractionsList/AttractionsList.js
@@ -1,18 +1,52 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import { Container } from './AttractionsList.styles'
+import lineHeart from '../../assets/image/line-heart.svg'
+import fillHeart from '../../assets/image/fill-heart.svg'
+import API from '../../apis/apis'
+import { useQuery } from '@tanstack/react-query'
 
-const AttractionsList = ({ data }) => {
+const AttractionsList = ({ data, index }) => {
+  const [fillLikeId, setFillLikeId] = useState('')
+  const [lineLikeId, setLineLikeId] = useState('')
+  const [isLike, setIsLike] = useState(data.like.isLiked)
+
+  useQuery(['likeId', fillLikeId], API.editAttractionLike, {
+    enabled: !!fillLikeId,
+  })
+  useQuery(['likeId', lineLikeId], API.deleteAttractionLike, {
+    enabled: !!lineLikeId,
+  })
+
+  const onLikeClick = useCallback(e => {
+    if (e.target.className === 'fillHeart') {
+      setLineLikeId(e.target.id)
+      setIsLike(prev => !prev)
+    }
+    if (e.target.className === 'lineHeart') {
+      setFillLikeId(e.target.id)
+      setIsLike(prev => !prev)
+    }
+  }, [])
+
   return (
-    <Container key={data.id}>
+    <Container>
       <img src={data.coverImageUrl} alt="thumbnail" />
       <div>
         <h1>{data.name}</h1>
         <p>{data.description}</p>
         <div>
-          <span>star: {data.reviews.averageRating}</span>
-          <span>count : {data.reviews.count}</span>
+          <span>{Math.ceil(data.reviews.averageRating * 2) / 2}</span>
+          <span>({data.reviews.count > 99 ? '99+' : data.reviews.count})</span>
         </div>
-        <span>{data.like.count}</span>
+        <span>{data.like.count > 999 ? '999+' : data.like.count}</span>
+        <img
+          onClick={onLikeClick}
+          id={data.id}
+          className={isLike ? 'fillHeart' : 'lineHeart'}
+          src={isLike ? fillHeart : lineHeart}
+          alt="Heart"
+          style={{ width: 20, height: 20 }}
+        />
       </div>
     </Container>
   )
--- src/components/AttractionsList/AttractionsList.styles.js
+++ src/components/AttractionsList/AttractionsList.styles.js
@@ -7,20 +7,23 @@ const Container = styled.div`
     font-size: 18px;
     font-weight: 700;
     margin-bottom: 4px;
+    display: -webkit-box;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    -webkit-line-clamp: 1;
+    -webkit-box-orient: vertical;
   }
   p {
-    display: block;
     font-size: 14px;
     font-weight: 500;
     margin-bottom: 5px;
     display: -webkit-box;
     overflow: hidden;
     text-overflow: ellipsis;
-    white-space: normal;
-    overflow-wrap: break-word;
     -webkit-line-clamp: 2;
     -webkit-box-orient: vertical;
   }
+
   img {
     margin-right: 16px;
     width: 92px;
