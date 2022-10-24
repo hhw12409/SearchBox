bffbf76c2397dd24e524c02b3a618bc91757f664
hhw12409 <sksk2425@naver.com>
2022-10-25 00:30:40 +0900

    Feat: edit, delete api

--- src/apis/apis.js
+++ src/apis/apis.js
@@ -8,10 +8,30 @@ const request = async config => {
 }
 
 const API = {
-  getAttraction: () => {
+  getAttraction: ({ queryKey }) => {
+    if (queryKey[1]) {
+      return request({
+        method: 'GET',
+        url: `${BASE_URL}?query=${queryKey[1]}`,
+      })
+    }
+    if (!queryKey[1]) {
+      return request({
+        method: 'GET',
+        url: `${BASE_URL}`,
+      })
+    }
+  },
+  editAttractionLike: ({ queryKey }) => {
     return request({
-      method: 'GET',
-      url: `${BASE_URL}`,
+      method: 'PUT',
+      url: `${BASE_URL}/${queryKey[1]}/like`,
+    })
+  },
+  deleteAttractionLike: ({ queryKey }) => {
+    return request({
+      method: 'DELETE',
+      url: `${BASE_URL}/${queryKey[1]}/like`,
     })
   },
 }
