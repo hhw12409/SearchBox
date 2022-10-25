23b978c3ea66cf808ee07d1a86d589e6bc45e3e6
hhw12409 <sksk2425@naver.com>
2022-10-25 19:14:31 +0900

    Docs: change redirect url

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -4,7 +4,7 @@ const NotFound = () => {
   return (
     <div>
       <h1>없는 페이지 입니다.</h1>
-      <Link to="/api/attractions">홈페이지로 돌아가기</Link>
+      <Link to="/">홈페이지로 돌아가기</Link>
     </div>
   )
 }
