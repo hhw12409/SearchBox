38c5be253a92ad84509916536601d83c6a21234c
hhw12409 <sksk2425@naver.com>
2022-10-25 19:03:03 +0900

    Docs: change api uri path

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,66 +1,66 @@
-import { rest } from "msw";
-import attractions from "./attractions.json";
+import { rest } from 'msw'
+import attractions from './attractions.json'
 
-const LIKES_STORAGE_KEY = "__triple_frontend_assignment_mock_api_key__likes";
+const LIKES_STORAGE_KEY = '__triple_frontend_assignment_mock_api_key__likes'
 
 export const handlers = [
-  rest.get("/api/attractions", async (req, res, ctx) => {
-    const query = req.url.searchParams.get("query");
-    const likes = JSON.parse(sessionStorage.getItem(LIKES_STORAGE_KEY) || "{}");
+  rest.get('/attractions', async (req, res, ctx) => {
+    const query = req.url.searchParams.get('query')
+    const likes = JSON.parse(sessionStorage.getItem(LIKES_STORAGE_KEY) || '{}')
 
     const filteredAttractions = query
       ? filterAttractionsWithQuery(attractions, query)
-      : attractions;
-    const isLikedAddedAttractions = filteredAttractions.map((attraction) => ({
+      : attractions
+    const isLikedAddedAttractions = filteredAttractions.map(attraction => ({
       ...attraction,
       like: {
         isLiked: likes[attraction.id] ?? false,
         count: attraction.like.count + (likes[attraction.id] ? 1 : 0),
       },
-    }));
+    }))
 
-    if (process.env.NODE_ENV !== "test") {
-      await sleep(800);
+    if (process.env.NODE_ENV !== 'test') {
+      await sleep(800)
     }
 
-    return res(ctx.status(200), ctx.json(isLikedAddedAttractions));
+    return res(ctx.status(200), ctx.json(isLikedAddedAttractions))
   }),
-  rest.put("/api/attractions/:attractionId/like", async (req, res, ctx) => {
-    const { attractionId } = req.params;
-    const likes = JSON.parse(sessionStorage.getItem(LIKES_STORAGE_KEY) || "{}");
+  rest.put('/attractions/:attractionId/like', async (req, res, ctx) => {
+    const { attractionId } = req.params
+    const likes = JSON.parse(sessionStorage.getItem(LIKES_STORAGE_KEY) || '{}')
 
-    likes[attractionId] = true;
+    likes[attractionId] = true
 
-    sessionStorage.setItem(LIKES_STORAGE_KEY, JSON.stringify(likes));
+    sessionStorage.setItem(LIKES_STORAGE_KEY, JSON.stringify(likes))
 
-    if (process.env.NODE_ENV !== "test") {
-      await sleep(400);
+    if (process.env.NODE_ENV !== 'test') {
+      await sleep(400)
     }
 
-    return res(ctx.status(204));
+    return res(ctx.status(204))
   }),
-  rest.delete("/api/attractions/:attractionId/like", async (req, res, ctx) => {
-    const { attractionId } = req.params;
-    const likes = JSON.parse(sessionStorage.getItem(LIKES_STORAGE_KEY) || "{}");
+  rest.delete('/attractions/:attractionId/like', async (req, res, ctx) => {
+    const { attractionId } = req.params
+    const likes = JSON.parse(sessionStorage.getItem(LIKES_STORAGE_KEY) || '{}')
 
-    likes[attractionId] = false;
+    likes[attractionId] = false
 
-    sessionStorage.setItem(LIKES_STORAGE_KEY, JSON.stringify(likes));
+    sessionStorage.setItem(LIKES_STORAGE_KEY, JSON.stringify(likes))
 
-    if (process.env.NODE_ENV !== "test") {
-      await sleep(400);
+    if (process.env.NODE_ENV !== 'test') {
+      await sleep(400)
     }
 
-    return res(ctx.status(204));
+    return res(ctx.status(204))
   }),
-];
+]
 
 function filterAttractionsWithQuery(attractions, query) {
-  return attractions.filter((attraction) => attraction.name.includes(query));
+  return attractions.filter(attraction => attraction.name.includes(query))
 }
 
 async function sleep(timeout) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, timeout);
-  });
+  return new Promise(resolve => {
+    setTimeout(resolve, timeout)
+  })
 }
