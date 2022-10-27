52e71a0c4ec17223b5968f509f63d6ebe870e9f2
hhw12409 <sksk2425@naver.com>
2022-10-27 20:52:49 +0900

    Feat: setting babel and jest

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  presets: [
+    '@babel/preset-env',
+    ['@babel/preset-react', { runtime: 'automatic' }],
+  ],
+  env: {
+    test: {
+      plugins: [
+        '@babel/plugin-transform-modules-commonjs',
+        '@babel/plugin-transform-runtime',
+        'inline-react-svg',
+      ],
+    },
+  },
+}
